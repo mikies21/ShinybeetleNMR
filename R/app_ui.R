fbc079fd3ecb18853f60e29277c83b869159e04e
mikies21 <67366122+mikies21@users.noreply.github.com>
2023-01-01 23:51:11 +0000

    changed UI to bs4Dash
    change the whole UI to bs4Dash.
    analysis inputs can be found on the right for better and faster analysis

--- R/app_ui.R
+++ R/app_ui.R
@@ -13,82 +13,103 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    shinydashboardPlus::dashboardPage(
-      header = shinydashboardPlus::dashboardHeader(title = "ShinybeetlesNMR"),
-      sidebar = shinydashboardPlus::dashboardSidebar(
+    
+    bs4Dash::dashboardPage(
+      title = "ShinybeetlesNMR",
+      fullscreen = TRUE,
+      header = bs4Dash::dashboardHeader(
+        title = "ShinybeetlesNMR",
+        skin = "light",
+        status = "white",
+        border = TRUE,
+        sidebarIcon = icon("bars"),
+        controlbarIcon = icon("th"),
+        fixed = FALSE
+        ),
+      sidebar = bs4Dash::dashboardSidebar(
+        skin = "light",
+        status = "primary",
+        elevation = 3,
         minified = TRUE,
-        collapsed = TRUE,
-        shinydashboard::sidebarMenu(
+        bs4Dash::sidebarMenu(
           id = "sidebar",
-          shinydashboard::menuItem(
+          bs4Dash::menuItem(
             text = "Start Here",
             tabName = "start",
             selected = T
           ),
-          shinydashboard::menuItem(
+          bs4Dash::menuItem(
             text = "Plot Spectra", 
             tabName = "PlotSpectra"
           ),
-          shinydashboard::menuItem(
-            text = "Analysis",
-            tabName = "Analysis",
-            icon = icon("dashboard"),
-            shinydashboard::menuSubItem(
-              tabName = "Analysis",
-              text = "inputs"
+          bs4Dash::sidebarHeader(title = "Analysis"),
+          bs4Dash::menuItem(
+            text = "home",
+            tabName = "home",
+            icon = icon("dashboard")
             ),
-            mod_Upload_data_ui("Upload_data_ui_1"),
-            shiny::uiOutput(outputId = "groupingvariable_global_UI"),
-            shiny::checkboxInput(inputId = "Q_filter_data", value = F, label = "filter dataframe"),
-            conditionalPanel(
-              condition = "input.Q_filter_data == true",
-              datamods::filter_data_ui(id = "filter", show_nrow = TRUE, max_height = NULL)
-            )
-
-            #          # selectInput(inputId = "addfiltermod", label = "filtering options", choices = c("a", "b"))
-            #        )),
-          )
+          bs4Dash::menuItem(
+            text = "Univariate",
+            tabName = "Univariate"),
+          bs4Dash::menuItem(
+            text = "PCA",
+            tabName = "PCA"),
+          bs4Dash::menuItem(
+            text = "PLS-DA",
+            tabName = "PLSDA")
         )
       ),
-      body = shinydashboard::dashboardBody(
-        shinydashboard::tabItems(
-          shinydashboard::tabItem(
+      
+      
+      bs4Dash::dashboardControlbar(
+        skin = "light",
+        pinned = TRUE,
+        overlay = FALSE,
+        collapsed = F, 
+        mod_Upload_data_ui("Upload_data_ui_1"),
+        
+        #          # selectInput(inputId = "addfiltermod", label = "filtering options", choices = c("a", "b"))
+        #        )),
+        shiny::uiOutput(outputId = "groupingvariable_global_UI"),
+        shiny::checkboxInput(inputId = "Q_filter_data", value = F, label = "filter dataframe"),
+        shiny::conditionalPanel(
+          condition = "input.Q_filter_data == true",
+          datamods::filter_data_ui(id = "filter", show_nrow = TRUE, max_height = NULL)
+          )
+        ),
+      
+      
+      body = bs4Dash::dashboardBody(
+        bs4Dash::tabItems(
+          bs4Dash::tabItem(
             tabName = "start",
             "start page, to be finished"
             ),
-          shinydashboard::tabItem(
+          bs4Dash::tabItem(
             tabName = "PlotSpectra",
             mod_spectra_plot_RAW_ui("spectra_plot_RAW_1")
           ),
-          shinydashboard::tabItem(
-            tabName = "Analysis",
-            shiny::tabsetPanel(
-              type = "tabs",
-              tabPanel(
-                title = "home",
-                shinydashboard::infoBoxOutput(outputId = "datadesc", width = 6),
-                shinydashboard::infoBoxOutput(outputId = "datatrans", width = 6),
-                mod_spectra_plot_ui("spectra_plot_ui_1")
-              ),
-              shiny::tabPanel(
-                title = "Univariate",
-                mod_Univariate_analysis_ui("Univariate_analysis_ui_1")
-              ),
-              shiny::tabPanel(
-                title = "PCA",
-                mod_PCA_ui("PCA_ui_1")
-              ),
-              shiny::tabPanel(
-                title = "PLS-DA",
-                mod_PLSDA_ui("PLSDA_ui_1")
-              )
+          bs4Dash::tabItem(
+            tabName = "home",
+            shinydashboard::infoBoxOutput(outputId = "datadesc", width = 6),
+            shinydashboard::infoBoxOutput(outputId = "datatrans", width = 6),
+            mod_spectra_plot_ui("spectra_plot_ui_1")
+            ),
+          bs4Dash::tabItem(
+            tabName =  "Univariate",
+            mod_Univariate_analysis_ui("Univariate_analysis_ui_1")
+            ),
+          bs4Dash::tabItem(
+            tabName = "PCA",
+            mod_PCA_ui("PCA_ui_1")
+            ),
+          bs4Dash::tabItem(
+            tabName = "PLSDA",
+            mod_PLSDA_ui("PLSDA_ui_1")
             )
           )
         )
-      ),
-      controlbar = shinydashboardPlus::dashboardControlbar(),
-      title = "ShinybeetlesNMR"
-    )
+      )
     ### title Page
     #    shiny::titlePanel(shiny::HTML("<center>ShinybeetlesNMR</center>")),
     #
@@ -156,7 +177,7 @@ golem_add_external_resources <- function() {
     ),
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
-    shinyWidgets::setBackgroundColor(color = "ghostwhite"),
-    shinyWidgets::useShinydashboard()
+    #shinyWidgets::setBackgroundColor(color = "ghostwhite"),
+    #shinyWidgets::useShinydashboard()
   )
 }
